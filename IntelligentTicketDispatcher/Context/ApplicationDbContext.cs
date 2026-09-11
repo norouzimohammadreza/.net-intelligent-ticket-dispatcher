01faf66289106af973e1d4079c26955333d09cb2
norouzimohammadreza <rda0044@gmail.com>
2026-09-11 12:41:50 +0330

    :)

---
+++ IntelligentTicketDispatcher/Context/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using IntelligentTicketDispatcher.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace IntelligentTicketDispatcher.Context;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {}
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<Admin> Admins { get; set; }
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<Department> Departments { get; set; }
+    public DbSet<Ticket> Tickets { get; set; }
+    public DbSet<AdminDepartment> AdminDepartments { get; set; }
+    public DbSet<AdminTicket> AdminTickets { get; set; }
+}
