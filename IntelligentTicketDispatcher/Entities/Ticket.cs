12c3f0d602111f3741d79ab1b527e07cd9e70784
norouzimohammadreza <rda0044@gmail.com>
2026-09-11 10:37:50 +0330

    feat: create ticket

---
+++ IntelligentTicketDispatcher/Entities/Ticket.cs
@@ -0,0 +1,28 @@
+﻿using IntelligentTicketDispatcher.Enums;
+
+namespace IntelligentTicketDispatcher.Entities;
+
+public class Ticket : BaseEntity
+{
+    public int CustomerId { get; private set; }
+    public Customer? Customer { get; private set; }
+    public string Subject { get; private set; }
+    public TicketPriority Priority { get; private set; }
+    public bool IsOpened { get; private set; } = true;
+    public int? DepartmentId { get; private set; }
+    public Department? Department { get; private set; }
+    public ICollection<AdminTickets> AdminTickets { get; private set; } = [];
+
+    private Ticket()
+    {
+    }
+
+    public Ticket(int customerId, string subject, TicketPriority priority, int? departmentId)
+    {
+        CustomerId = customerId;
+        Subject = subject;
+        Priority = priority;
+        DepartmentId = departmentId;
+        CreatedAt = DateTime.Now;
+    }
+}
---
+++ IntelligentTicketDispatcher/Enums/TicketPriority.cs
@@ -0,0 +1,9 @@
+﻿namespace IntelligentTicketDispatcher.Enums;
+
+public enum TicketPriority : int
+{
+    Low = 1,
+    Medium = 2,
+    High = 3,
+    Urgent = 4
+}
