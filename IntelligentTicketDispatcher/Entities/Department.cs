ae2f6f3b5710fe175fe7e695f2d24635f2343a48
norouzimohammadreza <rda0044@gmail.com>
2026-09-11 10:37:36 +0330

    feat: create department entity

---
+++ IntelligentTicketDispatcher/Entities/Department.cs
@@ -0,0 +1,20 @@
+﻿namespace IntelligentTicketDispatcher.Entities;
+
+public class Department : BaseEntity
+{
+    public string Name { get; set; }
+    public int? ParentDepartmentId { get; set; }
+    public Department? ParentDepartment { get; set; }
+    public ICollection<Department> ChildDepartments { get; private set; } = [];
+    public ICollection<AdminDepartments> AdminDepartments { get; private set; } = [];
+
+    private Department()
+    {
+    }
+
+    public Department(string name, int? parentDepartmentId)
+    {
+        Name = name;
+        ParentDepartmentId = parentDepartmentId;
+    }
+}
