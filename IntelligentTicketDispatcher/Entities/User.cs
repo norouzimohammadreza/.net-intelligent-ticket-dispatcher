8cffcacadcea8a28f1e5a1e89760f4aa84e0a84c
norouzimohammadreza <rda0044@gmail.com>
2026-09-11 10:37:00 +0330

    feat: create user entity

---
+++ IntelligentTicketDispatcher/Entities/User.cs
@@ -0,0 +1,33 @@
+﻿namespace IntelligentTicketDispatcher.Entities;
+
+public class User : BaseEntity
+{
+    public string Name { get; private set; }
+    public string Email { get; private set; }
+    public string Password { get; private set; }
+
+    private User()
+    {
+    }
+
+    public User(string name, string email, string password)
+    {
+        Name = name;
+        Email = email;
+        Password = password;
+        CreatedAt = DateTime.UtcNow;
+    }
+
+    public void Update(string name, string email)
+    {
+        Name = name;
+        Email = email;
+        UpdatedAt = DateTime.UtcNow;
+    }
+
+    public void UpdatePassword(string password)
+    {
+        Password = password;
+        UpdatedAt = DateTime.UtcNow;
+    }
+}
