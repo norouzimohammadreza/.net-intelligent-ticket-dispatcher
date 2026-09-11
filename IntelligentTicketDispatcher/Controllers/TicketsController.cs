0d40f7075f61436c2d3305a8f1a22c913b4de34c
norouzimohammadreza <rda0044@gmail.com>
2026-09-11 18:54:57 +0330

    feat: add answer ticket endpoint

--- IntelligentTicketDispatcher/Controllers/TicketsController.cs
+++ IntelligentTicketDispatcher/Controllers/TicketsController.cs
@@ -1,6 +1,5 @@
 ﻿using IntelligentTicketDispatcher.Contracts;
 using IntelligentTicketDispatcher.Dtos.Ticket;
-using IntelligentTicketDispatcher.Entities;
 using IntelligentTicketDispatcher.Extensions;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,7 +12,14 @@ public class TicketsController(ITicketService ticketService) : Controller
     public async Task<IActionResult> Create([FromBody] CreateTicketDto dto)
     {
         var userId = Request.HttpContext.GetUserId();
-        await ticketService.CreateTicket(dto, userId);
-        return Ok();
+        var result = await ticketService.CreateTicket(dto, userId);
+        return Ok(result);
+    }
+
+    public async Task<IActionResult> Answer([FromBody] string answer, int ticketId)
+    {
+        var userId = Request.HttpContext.GetUserId();
+        var result = await ticketService.Answer(answer, userId, ticketId);
+        return Ok(result);
     }
 }
